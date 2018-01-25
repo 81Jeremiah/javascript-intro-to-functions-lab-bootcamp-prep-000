f7e2feac7fc3aee276d0a2e6e2b62d852ab9265c
Jeremiah Lutes <jeremiahlutes@gmail.com>
2018-01-25 05:16:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,8 +14,9 @@ function sayHiToGrandma (string) {
   if (string.lowerCase() === string(){
   return "I can\'t hear you!";
 } 
-else if (string.upperCase() === string())
+else if (string.upperCase() === string(){
 return "YES INDEED";
+}
 else if 
 return "I love you, too."
 }
